43f6c5ebda9bf2ff269e1a0ba19b9f1b9b2e48a6
oldejollyroger <rsaladie31@gmail.com>
2026-02-28 11:56:37 +0100

    Delete CAT QUEST 2 directory

--- CAT QUEST 2/data.js
+++
@@ -1,6 +0,0 @@
-const STEAM_DATA = [
-  { "id": "ACH_01", "name": "Miau al platino", "desc": "Consigue todos los trofeos.", "icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/914710/832e55b54768c03e0b50f34d294e85af07efab66.jpg" },
-  { "id": "ACH_02", "name": "¡Que empiece la feliaventura!", "desc": "Completa tu primera misión secundaria.", "icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/914710/68132e4822709e85573752e259e2f9d659424888.jpg" },
-  { "id": "ACH_03", "name": "Salto de fe", "desc": "Aprende a caminar sobre el agua.", "icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/914710/2c7c5a0a382607e1559868846c2f9d659424888.jpg" },
-  { "id": "ACH_05", "name": "Llave de oro", "desc": "Consigue la llave de oro.", "icon": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/apps/914710/5b813e4822709e85573752e259e2f9d659424888.jpg" }
-];
